34b0d4e46776ab3cb2eba1f82582c067b9462eb1
yz5201214 <yz120821884@163.com>
2019-08-21 18:27:50 +0800

    2019年8月21日 18:27:46

--- DevelopMentLog/__init__.py
+++ DevelopMentLog/__init__.py
@@ -1,17 +1,34 @@
 import json,re,operator
+import pymysql
+import pandas as pd
+from redis import Redis
 
 if __name__ == '__main__':
-    abc = '[{"file_name": "The Untamed.2019.EP01-08.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_0.torrent", "file_size": "148.50K"}, {"file_name": "The Untamed.2019.EP09-10.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_1.torrent", "file_size": "82.55K"}, {"file_name": "The Untamed.2019.EP11-14.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_2.torrent", "file_size": "83.04K"}, {"file_name": "The Untamed.2019.EP15-20.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_3.torrent", "file_size": "121.67K"}, {"file_name": "The Untamed.2019.EP21-22.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_4.torrent", "file_size": "83.16K"}, {"file_name": "The Untamed.2019.EP23-24.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_5.torrent", "file_size": "82.90K"}, {"file_name": "The Untamed.2019.EP25-26.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_6.torrent", "file_size": "83.47K"}, {"file_name": "The Untamed.2019.EP27-28.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_7.torrent", "file_size": "83.23K"}, {"file_name": "The Untamed.2019.EP29-30.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_8.torrent", "file_size": "83.31K"}, {"file_name": "The Untamed.2019.EP31-32.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_9.torrent", "file_size": "83.64K"}, {"file_name": "The Untamed.2019.EP33-38.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_10.torrent", "file_size": "124.56K"}, {"file_name": "The Untamed.2019.EP39-42.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_11.torrent", "file_size": "85.38K"}, {"file_name": "The Untamed.2019.EP43-44.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_12.torrent", "file_size": "84.93K"}, {"file_name": "The Untamed.2019.EP45-46.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_13.torrent", "file_size": "86.42K"}, {"file_name": "The Untamed.2019.EP47-48.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_14.torrent", "file_size": "81.65K"}, {"file_name": "The Untamed.2019.EP49-50.WEB-DL.1080p.HEVC.AAC-HQC.torrent", "file_url": "drama/2019/thread-index-fid-950-tid-4495641.htm/thread-index-fid-950-tid-4495641.htm_15.torrent", "file_size": "83.47K"}]'
-    testa = json.loads(abc)
-    print(testa)
+    redis_db = Redis(host='dev.kuituoshi.com', port=6379, password='88888888', db=4)
+    redis_data_btbbt = 'btbbt_cache'
 
-    testa = ['2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2'
- '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '2' '1' '1' '1' '1' '1' '1'
- '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1'
- '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1'
- '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1' '1']
-    print(testa.index('2'))
+    my = pymysql.connect(
+        host='www.findbt.com',  # 数据库地址
+        port=3306,  # 端口 注意是int类型
+        db='ultrax',  # 数据库名称
+        user='root',  # 用户名
+        passwd='cyfU2ypeM9AVeVzh',  # 用户密码
+        charset='utf8',  # 字符编码集 ,注意这里，直接写utf8即可
+        use_unicode=True)
+    selectSQL = 'select * from pre_common_pluginvar'
+    common = pd.read_sql(selectSQL, my)  # 影视大全参数配置表
+    for commonItem in common.get_values():
+        sItem = pd.Series(commonItem)
+        if sItem[5] == 'yuyan':
+            for xItem in sItem[7].split('\r\n'):
+                abc = xItem.split('=')
+                redis_db.hset('yuyan', abc[1], abc[0])# 结构：key=国语，value=1
 
-
+    yuyan = []  # 其他
+    for rKye in redis_db.hkeys('yuyan'):
+        rValue = redis_db.hget('yuyan', rKye)
+        if '英语中字'.find(rKye.decode('utf-8')[0:1]) >= 0:
+            yuyan.append(rValue.decode('utf-8'))
+    print(','.join(yuyan))
 
 
